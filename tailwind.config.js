a9fea216503efa226e2ee696e7145c2be2897442
IshikawaTokyo <yyusb.mac@gmail.com>
2023-07-07 23:36:17 +0900

    新たにbackgroundcolorを設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
       blue: "#0F172A",
       transparent: "rgba(255, 255, 255, 0.1)",
       slate: "rgb(241 245 249)",
+      white: "rgb(255, 255, 255)",
     },
     borderRadius: {
       xl: "50px",
       md: "20px",
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
     },
   },
   plugins: [],
