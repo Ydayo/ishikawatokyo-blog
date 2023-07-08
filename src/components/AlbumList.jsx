8825bf0fa239ee0f7a6b9038960291d83dacd1dc
IshikawaTokyo <yyusb.mac@gmail.com>
2023-07-09 01:09:23 +0900

    react-pagination-barを使用しpagination完成

--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -1,65 +1,61 @@
-import React, { useEffect, useState } from "react";
-import ReactPaginate from "react-paginate";
+import React, { useState } from "react";
+import { blogs } from "../constants";
+import { Pagination } from "react-pagination-bar";
+import "react-pagination-bar/dist/index.css";
+import { getBlogs } from "../libs/microcms";
 
+// const response = await getBlogs({ fields: ["id", "title"] });
 const AlbumList = () => {
-  const [albums, setAlbums] = useState([]);
-  const [itemsOffset, setItemsOffset] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pagePostsLimit = 6;
 
-  const itemsPerPage = 6; //数字は任意に変えられる
-
-  const endOffset = itemsOffset + itemsPerPage;
-
-  const currentAlbums = albums.slice(itemsOffset, endOffset);
-  const pageCount = Math.ceil(albums.length / itemsPerPage);
-
-  const handlePageClick = (e) => {
-    const newOffset = (e.selected * itemsPerPage) % albums.length;
-    setItemsOffset(newOffset);
-  };
-
-  useEffect(() => {
-    // 非同期処理 / fetch関数
-    const getAlbums = async () => {
-      await fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
-        .then((res) => res.json())
-        .then((albums) => setAlbums(albums));
-    };
-    getAlbums();
-  }, []);
   return (
-    <div className="max-w-7xl h-full flex items-center flex-wrap justify-center gap-5 mx-auto">
-      {currentAlbums.map((album) => (
-        <div key={album.id}>
-          <img
-            src={album.url}
-            alt="album"
-            width={400}
-            currentAlbums={currentAlbums}
-          />
+    <div className="max-w-7xl h-full flex items-center flex-wrap justify-center gap-5 sm:mx-auto mx-3">
+      {blogs
+        .slice((currentPage - 1) * pagePostsLimit, currentPage * pagePostsLimit)
+        .map((blog, idx) => (
+          <div key={idx} className="bg-rose h-[400px] w-[400px]">
+            <img
+              src={blog.image}
+              alt="blog"
+              width={350}
+              height={300}
+              // currentblogs={currentBlogs}
+            />
+            <p>{blog.title}</p>
+            <p>{blog.desc}</p>
+          </div>
+        ))}
+      <div className="w-full flex justify-center">
+        <Pagination
+          currentPage={currentPage}
+          itemsPerPage={pagePostsLimit}
+          onPageChange={(pageNumber) => setCurrentPage(pageNumber)}
+          totalItems={blogs.length}
+          pageNeighbours={2}
+        />
+      </div>
+      {/* {response.contents.map((blog) => (
+        <div>
+          {blog.image}
+          <a href={blog.id} className="text-white">
+            {blog.title}
+          </a>
         </div>
-      ))}
-      <ReactPaginate
-        className="text-white w-full flex justify-center"
-        pageCount={pageCount}
-        onPageChange={handlePageClick}
-        nextLabel="next >"
-        pageRangeDisplayed={3}
-        marginPagesDisplayed={2}
-        previousLabel="< previous"
-        pageClassName="page-item"
-        pageLinkClassName="page-link"
-        previousClassName="page-item"
-        previousLinkClassName="page-link"
-        nextClassName="page-item"
-        nextLinkClassName="page-link"
-        breakLabel="..."
-        breakClassName="page-item"
-        breakLinkClassName="page-link"
-        containerClassName="pagination"
-        activeClassName="active"
-      />
+      ))} */}
     </div>
   );
 };
 
+// export const getStaticProps = async () => {
+//   const data = await client.get({
+//     endpoint: blogs,
+//   });
+
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// };
 export default AlbumList;
