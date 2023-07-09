1087eaebe9005ee01a45092ffb254432ab07669d
IshikawaTokyo <yyusb.mac@gmail.com>
2023-07-09 22:52:16 +0900

    カスタム

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,13 @@ module.exports = {
     backgroundColor: {
       blue: "#0F172A",
       transparent: "rgba(255, 255, 255, 0.1)",
+      transparent2: "rgba(255, 255, 255, 0.8)",
       slate: "rgb(241 245 249)",
       white: "rgb(255, 255, 255)",
       rose: "rgb(244, 63, 94)",
       orange: "#EE9B01",
       darkOrange: "#DA6A00",
+      paleblue: "#806491",
     },
     // colors: {
     //   white: "#fff",
